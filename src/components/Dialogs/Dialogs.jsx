3d40552188ce2a68d74255042ca4dc7bf1026fba
KasyanAndrey <andreykasyan88@gmail.com>
2022-02-07 23:58:41 +0200

    create cmpnt Dialogs

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,8 +1,34 @@
 import React from "react";
-// import css from "./Dialogs.module.css";
+import css from "./Dialogs.module.css";
 
 const Dialogs = (props) => {
-  return <div>Dialogs</div>;
+  return (
+    <div>
+      <div className={css.dialogs}>
+        <div className={css.dialogsList}>
+          <div className={css.dialog}>Jax</div>
+          <div className={css.dialog}>Mango</div>
+          <div className={css.dialog}>Poli</div>
+          <div className={css.dialog}>Ajax</div>
+          <div className={css.dialog}>Baz</div>
+          <div className={css.dialog}>Congo</div>
+        </div>
+        <div className={css.messagesList}>
+          <div className={css.message}>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Qui
+            repellat error reprehenderit nam consequuntur doloremque.
+          </div>
+          <div className={css.message}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates,
+            animi!
+          </div>
+          <div className={css.message}>
+            Lorem ipsum dolor sit amet consectetur.
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Dialogs;
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -6,21 +6,50 @@ import css from "./Navbar.module.css";
 const Navbar = () => {
   return (
     <nav className={css.nav}>
-      <div className={`${css.link} ${css.active}`}>
-        <NavLink to="/profile">Profile</NavLink>
-      </div>
-      <div className={css.link}>
-        <NavLink to="/dialogs">Messages</NavLink>
-      </div>
-      <div className={css.link}>
-        <NavLink to="/news">News</NavLink>
-      </div>
-      <div className={css.link}>
-        <NavLink to="/music">Music</NavLink>
-      </div>
-      <div className={css.link}>
-        <NavLink to="/settings">Settings</NavLink>
-      </div>
+      <NavLink
+        to="/profile"
+        className={(navData) =>
+          navData.isActive ? `${css.activeLink} ${css.link}` : `${css.link}`
+        }
+      >
+        Profile
+      </NavLink>
+
+      <NavLink
+        to="/dialogs"
+        className={(navData) =>
+          navData.isActive ? `${css.activeLink} ${css.link}` : `${css.link}`
+        }
+      >
+        Messages
+      </NavLink>
+
+      <NavLink
+        to="/news"
+        className={(navData) =>
+          navData.isActive ? `${css.activeLink} ${css.link}` : `${css.link}`
+        }
+      >
+        News
+      </NavLink>
+
+      <NavLink
+        to="/music"
+        className={(navData) =>
+          navData.isActive ? `${css.activeLink} ${css.link}` : `${css.link}`
+        }
+      >
+        Music
+      </NavLink>
+
+      <NavLink
+        to="/settings"
+        className={(navData) =>
+          navData.isActive ? `${css.activeLink} ${css.link}` : `${css.link}`
+        }
+      >
+        Settings
+      </NavLink>
     </nav>
   );
 };

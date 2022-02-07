7c2b0b4cbd5ef79f26801880c577b47b854cfb23
KasyanAndrey <andreykasyan88@gmail.com>
2022-02-08 01:03:55 +0200

    chng cmpnt dialogs and add styles css

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ const App = (props) => {
     <div>
       <BrowserRouter>
         <div className="app-wrapper">
-          next 20
+          next 22
           <Header />
           <Navbar />
           <section className="app-wrapper-content">
--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,17 +1,36 @@
 import React from "react";
 import css from "./Dialogs.module.css";
+import imgAvatar from "../../assets/img/default_avatar.png";
 
 const Dialogs = (props) => {
   return (
     <div>
       <div className={css.dialogs}>
         <div className={css.dialogsList}>
-          <div className={css.dialog}>Jax</div>
-          <div className={css.dialog}>Mango</div>
-          <div className={css.dialog}>Poli</div>
-          <div className={css.dialog}>Ajax</div>
-          <div className={css.dialog}>Baz</div>
-          <div className={css.dialog}>Congo</div>
+          <div className={`${css.activeLink} ${css.dialog}`}>
+            <img src={imgAvatar} alt="user avatar"></img>
+            Jax
+          </div>
+          <div className={css.dialog}>
+            <img src={imgAvatar} alt="user avatar"></img>
+            Mango
+          </div>
+          <div className={css.dialog}>
+            <img src={imgAvatar} alt="user avatar"></img>
+            Poli
+          </div>
+          <div className={css.dialog}>
+            <img src={imgAvatar} alt="user avatar"></img>
+            Ajax
+          </div>
+          <div className={css.dialog}>
+            <img src={imgAvatar} alt="user avatar"></img>
+            Baz
+          </div>
+          <div className={css.dialog}>
+            <img src={imgAvatar} alt="user avatar"></img>
+            Congo
+          </div>
         </div>
         <div className={css.messagesList}>
           <div className={css.message}>

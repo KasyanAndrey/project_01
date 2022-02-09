0039f2968c626599024c6f24ca056a39c0237359
KasyanAndrey <andreykasyan88@gmail.com>
2022-02-09 23:50:24 +0200

    chng cmpnt Dialogs, create cmpnt Dialog/Message

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,49 +1,43 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 import css from "./Dialogs.module.css";
-import imgAvatar from "../../assets/img/default_avatar.png";
+
+const DialogItem = (props) => {
+  let path = "/dialogs/" + props.id;
+
+  return (
+    <div className={`${css.activeLink} ${css.dialog}`}>
+      <NavLink to={path}>{props.name}</NavLink>
+    </div>
+  );
+};
+
+const Message = (props) => {
+  return <div className={css.message}>{props.message}</div>;
+};
 
 const Dialogs = (props) => {
   return (
     <div>
       <div className={css.dialogs}>
         <div className={css.dialogsList}>
-          <div className={`${css.activeLink} ${css.dialog}`}>
-            <img src={imgAvatar} alt="user avatar"></img>
-            Jax
-          </div>
-          <div className={css.dialog}>
-            <img src={imgAvatar} alt="user avatar"></img>
-            Mango
-          </div>
-          <div className={css.dialog}>
-            <img src={imgAvatar} alt="user avatar"></img>
-            Poli
-          </div>
-          <div className={css.dialog}>
-            <img src={imgAvatar} alt="user avatar"></img>
-            Ajax
-          </div>
-          <div className={css.dialog}>
-            <img src={imgAvatar} alt="user avatar"></img>
-            Baz
-          </div>
-          <div className={css.dialog}>
-            <img src={imgAvatar} alt="user avatar"></img>
-            Congo
-          </div>
+          <DialogItem name="Jax" id="1" />
+          <DialogItem name="Mango" id="2" />
+          <DialogItem name="Poli" id="3" />
+          <DialogItem name="Ajax" id="4" />
+          <DialogItem name="Baz" id="5" />
+          <DialogItem name="Congo" id="6" />
         </div>
         <div className={css.messagesList}>
-          <div className={css.message}>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Qui
-            repellat error reprehenderit nam consequuntur doloremque.
-          </div>
-          <div className={css.message}>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates,
-            animi!
-          </div>
-          <div className={css.message}>
-            Lorem ipsum dolor sit amet consectetur.
-          </div>
+          <Message
+            message="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Qui
+            repellat error reprehenderit nam consequuntur doloremque."
+          />
+          <Message message="Lorem ipsum dolor sit amet consectetur." />
+          <Message
+            message="Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates,
+            animi!"
+          />
         </div>
       </div>
     </div>
